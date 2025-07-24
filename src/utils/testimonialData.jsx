aa18d9acab4e117eb7f60cf95a2b39002ce622d7
pgslngcom <development@pgslng.com>
2025-07-24 10:27:56 +0100

    updated the bug on the testimonial

--- src/utils/testimonialData.jsx
+++ src/utils/testimonialData.jsx
@@ -1,5 +1,6 @@
 import testimonial1 from '../assets/Testimonial-pic1.jpg'
-import testimonial2 from '../assets/Testimonial-pic2.jpg'
+import testimonial2 from '../assets/Testimonial-pic3.jpg'
+import testimonial3 from '../assets/Testimonial-pic2.jpg'
 
 
 export const testimonial =[
@@ -14,19 +15,19 @@ export const testimonial =[
     },
     {
         id: 2,
-        name: 'Ibeleme Green',
+        name: 'Jacob Beskeni',
        star: "⭐⭐⭐⭐",
        location: 'Port Harcourt',
        img: testimonial2,
         text: 
-        'So, sleep is important  to me because my schedule  is pretty intense. When I heard someone had silk sheets in Port Harcourt. I immediately  jumped at the opportunity, and I can say for a fact that if my other bedsheets could talk. There would be a riot going on by now.Her quality is excellent, and her customer service and delivery are topnotch. Definitely  your go to bed accessories person',
+        'I have been shopping from Swishliving since 2022. I’ve also recommended their services to my sphere of influence. I love beige and white sleep essentials and they’ve always supplied to tooth',
     },
     {
-        id: 2,
+        id: 3,
         name: 'Ibeleme Green',
        star: "⭐⭐⭐⭐",
        location: 'Port Harcourt',
-       img: testimonial2,
+       img: testimonial3,
         text: 
         'So, sleep is important  to me because my schedule  is pretty intense. When I heard someone had silk sheets in Port Harcourt. I immediately  jumped at the opportunity, and I can say for a fact that if my other bedsheets could talk. There would be a riot going on by now.Her quality is excellent, and her customer service and delivery are topnotch. Definitely  your go to bed accessories person',
     },
