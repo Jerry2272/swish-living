36f64fee9953c8dae301a89c2752072e4ca25e52
pgslngcom <development@pgslng.com>
2025-07-25 12:05:50 +0100

    update the cart details page.

--- pages/ProductDetails.jsx
+++ pages/ProductDetails.jsx
@@ -79,7 +79,7 @@ const ProductDetails = () => {
 
       {/* All Products */}
       <div className="max-w-6xl mx-auto mt-10 px-6">
-        <h2 className="text-2xl font-bold mb-4 text-center">Explore More Products</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Explore More Products.</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {productItem.map((item) => (
             <ProductCard key={item.id} product={item} />
